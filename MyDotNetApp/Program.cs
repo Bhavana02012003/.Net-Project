a201a13eecaf202a9b5b7469b9db9ea28bc48242
Bhavana02012003 <edurupakabhavana2003@gmail.com>
2025-06-26 13:53:50 +0530

    .Net CodeScan

---
+++ MyDotNetApp/Program.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace MyDotNetApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("🔒 Hello, Secure .NET World!");
+            
+            string userInput = Console.ReadLine(); // 🛑 Potential security issue (SecurityCodeScan will flag this)
+            Console.WriteLine("User input: " + userInput);
+        }
+    }
+}
